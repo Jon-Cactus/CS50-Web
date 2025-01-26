a781b7a9f335fd06c9409a0214b3c36a168ee9ed
Jon-Cactus <jonathantcspence@gmail.com>
2025-01-26 12:40:40 +0900

    finished results view function

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("<str:entry>", views.generate_entry, name="entry"),
-    path("error/<str:search_query>", views.error, name="error"),
+    path("<str:entry_title>", views.generate_entry, name="entry"),
+    path("results/<str:search_query>", views.results, name="results"),
     path("add_entry", views.add_entry, name="add_no_query"),
     path("add_entry/<str:search_query>", views.add_entry, name="add"),
-    path("save/<str:entry_name", views.save, name="save")
+    path("save/<str:entry_name", views.add_entry, name="save")
 ]
--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse
-
+import markdown2
 from . import util
 
 #TODO check variable names for potential refactors
@@ -10,24 +10,23 @@ def index(request):
         entries = util.list_entries()
         search_query = request.GET.get('q')
         if search_query not in entries:
-            return redirect(reverse('error', args=[search_query]))
+            return redirect(reverse('results', args=[search_query]))
         else:
             return redirect(reverse('entry', args=[search_query]))
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
-"""
-Need to complete generate_entry before index can be properly displayed
-Next step: Create the html page for entries
-"""
-def generate_entry(request, entry):
-    return render(request, f"encyclopedia/entry.html", {
-        "entry": entry
+def generate_entry(request, entry_title):
+    entry_content = util.get_entry(entry_title)
+    return render(request, "encyclopedia/entry.html", {
+        "entry_content": markdown2.markdown(entry_content)
     })
 
 def add_entry(request, search_query=None):
-    #TODO create an add.html and correctly tie it to the error view and error.html
+    if 'entry_text' in request.POST:
+        entry_text = request.POST.get('entry_text')
+        util.save_entry(search_query, entry_text)
     if search_query is not None:
         return render(request, "encyclopedia/add.html", {
             "search_query": search_query
@@ -35,12 +34,19 @@ def add_entry(request, search_query=None):
     else:
         return render(request, "encyclopedia/add.html")
 
-def error(request, search_query=None):
-    #TODO: create a redirect to add a page with the search query
-
-    return render(request, "encyclopedia/error.html", {
-        "search_query": search_query
-    })
-
-def save(request):
-    return False
+def results(request, search_query=None):
+    #TODO: List entries that fit the search query as a substring
+    results = []
+    entries = util.list_entries()
+    for entry in entries:
+        if search_query.lower() in entry.lower():
+            results.append(entry)
+    if len(results) > 0:
+        return render(request, "encyclopedia/results.html", {
+            "search_query": search_query,
+            "results": results
+        })
+    else:
+        return render(request, "encyclopedia/results.html", {
+            "search_query": search_query
+        })
