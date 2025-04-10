51038913d33618c7d7b2716004ef6891f31b698f
Jon-Cactus <jonathantcspence@gmail.com>
2025-04-10 09:21:28 +0900

    initial commit. stable

---
+++ week7/project4/network/models.py
@@ -0,0 +1,19 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    following = models.ManyToManyField("self", symmetrical=False, blank=True, related_name="followers")
+    saved_posts = models.ManyToManyField("Post", blank=True, related_name="saved_by")
+
+class Post(models.Model):
+    user = models.ForeignKey("User", on_delete=models.CASCADE, related_name="user_posts")
+    content = models.TextField(max_length=512, blank=False, null=False)
+    likes = models.ManyToManyField("User", blank=True, related_name="liked_posts")
+    timestamp = models.TimeField(auto_now_add=True)
+
+class Comment(models.Model):
+    user = models.ForeignKey("User", on_delete=models.CASCADE)
+    post = models.ForeignKey("Post", on_delete=models.CASCADE)
+    text = models.CharField(blank=False, null=False, max_length=512)
+    timestamp = models.TimeField(auto_now_add=True)
