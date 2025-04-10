59df0f37d8c416333d095b11bc2b3d94728b3adb
Jon-Cactus <jonathantcspence@gmail.com>
2025-04-10 23:35:14 +0900

    form submission not working

--- week7/project4/network/admin.py
+++ week7/project4/network/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import User, Post, Comment
 
 # Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("user", "content", "likes", "timestamp")
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("username", "following")
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("user", "post", "text", "timestamp")
+
+admin.site.register(User)
+admin.site.register(Post)
+admin.site.register(Comment)
--- week7/project4/network/views.py
+++ week7/project4/network/views.py
@@ -1,11 +1,35 @@
+import json
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from django.db import IntegrityError
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 from django.shortcuts import render
 from django.urls import reverse
+from django.views.decorators.csrf import csrf_exempt
 
-from .models import User
+from .models import User, Post, Comment
 
+@csrf_exempt
+@login_required
+def post(request):
+    if request.method != "POST":
+        return JsonResponse({"error": "POST request required."}, status=400)
+    # retrieve information from post form
+    data = json.loads(request.body)
+    
+    content = data.get("content", "")
+    if not content: # check that post body is not empty
+        return JsonResponse({"error": "Can't share empty posts!"}, status=400)
+    # set post information
+    post = Post(user=request.user, content=content)
+    try:
+        post.save()
+        print(f"Post saved with ID: {post.id}")
+    except Exception as e:
+        print(f"Error saving post: {e}")
+        return JsonResponse({"error": "Failed to save post"}, status=500)
+
+    return JsonResponse({"message": "Post shared successfully."}, status=201)
 
 def index(request):
     return render(request, "network/index.html")
